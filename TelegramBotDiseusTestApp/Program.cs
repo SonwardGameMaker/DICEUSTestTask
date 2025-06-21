aa4b8b7347e3700d339c455596bad2337c3b02b9
SonwardGameMaker <sonward.gamemaker@gmail.com>
2025-06-21 11:02:00 +0300

    Update Program.cs

--- TelegramBotDiseusTestApp/Program.cs
+++ TelegramBotDiseusTestApp/Program.cs
@@ -11,14 +11,14 @@ builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 builder.Services.AddSingleton<ITelegramBotClient>(_ =>
     new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_BOT_API_KEY")));
 
-builder.Services.AddSingleton<AiChatManager>(_ => new AiChatManager(5));
+builder.Services.AddSingleton(_ => new AiChatManager(5));
 
 builder.Services.AddSingleton<TelegramDataTransferService>();
 
-builder.Services.AddSingleton<MindeeService>(_ =>
+builder.Services.AddSingleton(_ =>
     new MindeeService(Environment.GetEnvironmentVariable("MINDEE_API_KEY")));
 
-builder.Services.AddSingleton<GroqService>(_ =>
+builder.Services.AddSingleton(_ =>
     new GroqService(Environment.GetEnvironmentVariable("GROQ_API_KEY"), GroqModel.LLaMA3_70b));
 
 var app = builder.Build();
