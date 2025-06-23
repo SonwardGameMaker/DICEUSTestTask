4b675b663de105f5dfaa023efd4094cba50b5b49
SonwardGameMaker <sonward.gamemaker@gmail.com>
2025-06-23 17:14:23 +0300

    Cleaning

--- TelegramBotDiseusTestApp/Program.cs
+++ TelegramBotDiseusTestApp/Program.cs
@@ -7,16 +7,14 @@ using TelegramBotDiseusTestApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- Configuration ---
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 var telegramApiKey = Environment.GetEnvironmentVariable("TELEGRAM_BOT_API_KEY");
 var mindeeApiKey = Environment.GetEnvironmentVariable("MINDEE_API_KEY");
 var groqApiKey = Environment.GetEnvironmentVariable("GROQ_API_KEY");
 
-// --- Host ---
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
     
-// --- Services ---
 builder.Services.AddSingleton<ITelegramBotClient>(_ =>
     new TelegramBotClient(telegramApiKey));
 
@@ -30,7 +28,6 @@ builder.Services.AddSingleton<TelegramDataTransferService>();
 builder.Services.AddSingleton(_ => new MindeeService(mindeeApiKey));
 builder.Services.AddSingleton(_ => new GroqService(groqApiKey, GroqModel.LLaMA3_70b));
 
-// --- App ---
 var app = builder.Build();
 
 app.MapMethods("/", new[] { "GET", "HEAD" }, () => Results.Ok());
